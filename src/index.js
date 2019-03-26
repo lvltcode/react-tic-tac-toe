658fe36af2619152ddc2ae23c718ec309bfd0922
lvltcode <lvltcode@gmail.com>
2019-03-26 15:46:08 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -157,8 +157,8 @@ function calculateWinner(squares) {
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
       return squares[a];
     }
-    return null;
   }
+  return null;
 }
 
 // function calculateWinner(squares) {
